30f8bfd85f38b827b322e489cc59d8336a14b264
bayazid_sustami <bayazidsustamy@gmail.com>
2022-06-02 21:39:31 +0800

    feat : add string operation

--- data-type/simple_data_type2.py
+++ data-type/simple_data_type2.py
@@ -22,3 +22,11 @@ print(x1)
 binatang = ['kucing', 'rusa', 'badak', 'gajah']
 del binatang[2]
 print(binatang)
+
+s = "Hello World!"
+print(s[4])  # ambil karakter kelima dari string s
+print(s[6:11])  # ambil karakter ketujuh hingga sebelas dari string s
+s[5] = "d"  # ubah karakter keenam dari string s menjadi "d", seharusnya gagal karena immutable
+# ubah isi string s menjadi "Halo Dunia!", seharusnya berhasil karena mutable
+s = "Halo Dunia!"
+print(s)
