f720ce8062986ce7f639398461ef6b49ad613cf6
bayazid_sustami <bayazidsustamy@gmail.com>
2022-06-03 20:49:48 +0800

    feat : add conversion data-type

---
+++ data-type/convert.py
@@ -0,0 +1,22 @@
+aInteger = 5
+aIntegerToFloat = float(aInteger)
+print(aIntegerToFloat)
+
+aFloat = 3.2
+aFloatToInteger = int(aFloat)
+print(aFloatToInteger)
+
+aString = "3.5"
+aStringToFloat = float(aString)
+print(aStringToFloat)
+
+aIntegerToString = str(aInteger)
+print(aIntegerToString)
+
+# print(int('1p')) can be produce error
+
+print(set([1, 2, 3]))
+print(tuple({5, 6, 7}))
+print(list('hello'))
+print(dict([[1, 2], [3, 4]]))
+print(dict([(3, 26), (4, 44)]))
