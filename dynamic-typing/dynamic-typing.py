cd7c0c2375cc462c23d88fcf0edfd9720e1d8315
bayazid_sustami <bayazidsustamy@gmail.com>
2022-06-03 21:26:54 +0800

    feat : dynamic typing

---
+++ dynamic-typing/dynamic-typing.py
@@ -0,0 +1,10 @@
+# Ketika kita memberikan nilai 6 pada variabel x
+x = 6
+print(type(x))
+# Kemudian Berikan string “hello” pada variabel x di baris selanjutnya
+x = 'hello'
+print(type(x))
+
+# len() not applicable to int
+# x=546
+# print(len(x))
