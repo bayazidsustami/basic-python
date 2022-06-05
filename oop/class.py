c5a5bb3098a3b78162a5d7bb3cc900bebc517a72
bayazid_sustami <bayazidsustamy@gmail.com>
2022-06-05 23:54:09 +0800

    feat : class and method

---
+++ oop/class.py
@@ -0,0 +1,24 @@
+class Kalkulator:
+    """contoh kelas kalkulator sederhana"""
+
+    def __init__(self, i=12345):
+        self.i = i
+
+    def f(self):
+        return 'hello world'
+
+    @classmethod
+    def tambah_angka(cls, angka1, angka2):
+        return "{} + {} = {}".format(angka1, angka2, angka1+angka2)
+
+    @staticmethod
+    def kali_angka(angka1, angka2):
+        return '{} x {} = {}'.format(angka1, angka2, angka1 * angka2)
+
+
+# create instance
+calc = Kalkulator(i=1024)
+print(calc.i)
+print(calc.f())
+print(calc.tambah_angka(1, 2))
+print(Kalkulator.kali_angka(1, 2))
